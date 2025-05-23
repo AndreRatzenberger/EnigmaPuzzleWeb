ce0350f1d55c30bef3ce16607ac6f7eb44bde0a7
AndreRatzenberger <44863088+AndreRatzenberger@users.noreply.github.com>
2025-05-23 14:13:21 +0200

    Scaffold basic project structure for EnigmaPuzzleWeb

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Enigma Puzzle Web!</h1>
+      <p>A novel puzzle game experience is coming soon.</p>
+    </div>
+  );
+}
+export default App;
